360dd99b5319d2e3b988c6ab7ce7b4f52111fb27
Elizabeth Mathew <43873574+zabathm@users.noreply.github.com>
2019-01-31 09:19:09 +0530

    Add files via upload

---
+++ getCity.php
@@ -0,0 +1,16 @@
+<?php
+require_once ("dbcontroller.php");
+$db_handle = new DBController();
+if (! empty($_POST["state_id"])) {
+    $query = "SELECT * FROM district WHERE sid = '" . $_POST["state_id"] . "'";
+    $results = $db_handle->runQuery($query);
+    ?>
+<option value disabled selected>Select District</option>
+<?php
+    foreach ($results as $city) {
+        ?>
+<option value="<?php echo $city["did"]; ?>"><?php echo $city["dname"]; ?></option>
+<?php
+    }
+}
+?>
---
+++ getState.php
@@ -0,0 +1,16 @@
+<?php
+require_once ("dbcontroller.php");
+$db_handle = new DBController();
+if (! empty($_POST["country_id"])) {
+    $query = "SELECT * FROM state WHERE cid = '" . $_POST["country_id"] . "'";
+    $results = $db_handle->runQuery($query);
+    ?>
+<option>Select State</option>
+<?php
+    foreach ($results as $state) {
+        ?>
+<option value="<?php echo $state["sid"]; ?>"><?php echo $state["sname"]; ?></option>
+<?php
+    }
+}
+?>
