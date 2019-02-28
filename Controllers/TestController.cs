ec2a04f3ae3e91f1d828aabf0d118ca263d0c1d2
Elizabeth Mathew <43873574+zabathm@users.noreply.github.com>
2019-02-28 16:33:26 +0530

    DotNet MVC Framework
    28/02/2019

---
+++ Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Myproject.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getString()
+        {
+            return View();
+        }
+        public ActionResult getStudent()
+        {
+            return View();
+        }
+    }
+}
